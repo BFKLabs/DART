ddb0c5e35c0e126db87286b72e906fad65bb59a3
RichardFav <richard.faville@gmail.com>
2021-10-01 12:35:34 +0200

    Java object detection fix

--- Code/Common/Formatting/getTabGroupJavaObj.m
+++ Code/Common/Formatting/getTabGroupJavaObj.m
@@ -1,5 +1,22 @@
 % --- retrieves the java object handle from a tab group
 function jTab = getTabGroupJavaObj(hTabGrp)
 
-jTab0 = findjobj(hTabGrp);
-jTab = jTab0(arrayfun(@(x)(strContains(class(x),'MJTabbedPane')),jTab0));
+% removes the warnings
+wState = warning('off','all');
+
+% attempts to retrieves the table group java object
+cType = 'MJTabbedPane';
+jTab = findjobj(hTabGrp,'class',cType);
+
+% if no match was made, then return all the java objects for search
+if isempty(jTab)
+    % retrieves all the java objects
+    [~,~,~,~,handlesAll] = findjobj(hTabGrp);
+    
+    % retrieves the tabbed pane object
+    objClass = arrayfun(@(x)(class(x)),handlesAll,'un',0)';
+    jTab = handlesAll(strContains(objClass,cType));
+end
+
+% resets the warning state
+warning(wState)
